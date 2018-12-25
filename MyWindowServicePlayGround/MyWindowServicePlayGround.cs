d9ad40ee5fd13f797c4c797e7b4937a814b17e72
yong <richardleesh@gmail.com>
2018-12-25 12:02:28 -0500

    3rd commit

---
+++ MyWindowServicePlayGround/MyWindowServicePlayGround.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Configuration;
+using System.Timers;
+
+namespace MyWindowServicePlayGround
+{
+    public partial class MyWindowServicePlayGround : ServiceBase
+    {
+        private Task _playGroundServiceTask;
+
+        public MyWindowServicePlayGround()
+        {
+            InitializeComponent();
+
+            MyWindowsServicePlayGourndLog = new EventLog();
+
+            if (!EventLog.SourceExists("My Windows Service Play Ground Service")) ;
+            {
+                EventLog.CreateEventSource("My Windows Service Play Ground Service", "MyWindowsServicePlayGourndLog");
+            }
+
+            MyWindowsServicePlayGourndLog.Source = "My Windows Service Play Ground Service";
+            MyWindowsServicePlayGourndLog.Log = "";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Thread.Sleep(10000);
+            int playGoundServiceScanInterval = 3600000;
+            if (!(string.IsNullOrEmpty(ConfigurationManager.AppSettings["PlayGoundServiceScanInterval"])))
+            {
+                if(!int.TryParse(ConfigurationManager.AppSettings["PlayGoundServiceScanInterval"],out playGoundServiceScanInterval))
+                {
+                    playGoundServiceScanInterval = 3600000;
+                }
+            }
+
+            System.Timers.Timer taskScanTimer = new System.Timers.Timer();
+            taskScanTimer.Interval = playGoundServiceScanInterval;
+            taskScanTimer.Elapsed += new System.Timers.ElapsedEventHandler(this.OnTaskTimer);
+            taskScanTimer.Start();
+        }
+
+        private void OnTaskTimer(object sender, ElapsedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
